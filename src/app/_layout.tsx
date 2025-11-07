b0867f483f2d1baeeb72cd67af93563158ee32e6
DaviPereiraJs <davi.nascimento182446@gmail.com>
2025-11-07 20:53:04 -0300

    feat: Adicionando mais arquivos

---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import {} from '../styles/global.css'
+import { Slot } from "expo-router"
+
+export default function RootLayout() {
+  return <Slot />;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, ScrollView } from "react-native";
+import { Header } from "../header/header";
+import Constants from 'expo-constants'
+import { Banner } from "../banner";
+
+const statusBarHeight = Constants.statusBarHeight;
+
+export default function Index() {
+  return (
+      <ScrollView
+      style={{ flex: 1 }} 
+      className="bg-slate-200" 
+      showsVerticalScrollIndicator={false}>
+
+        <View className="w-full px-4 " style={{ marginTop: statusBarHeight + 8 }}>
+          <Header/>
+
+          <Banner/>
+        </View>
+
+      </ScrollView>
+  );
+}
